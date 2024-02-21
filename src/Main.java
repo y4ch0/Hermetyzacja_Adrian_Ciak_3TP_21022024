98d3b836c664d65507cddca4cce0d337b545022b
y4ch0 <adrianciak10@gmail.com>
2024-02-21 10:29:56 +0100

    add more objects to main class

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,15 @@ public class Main {
         accOne.getClientInfo();
         accOne.updtAccBal(1234,329.99);
         accOne.updtPin(1234,4321);
+        Client clTwo = new Client("Adrian","","Ciak",18,"2006-03-03");
+        Account accTwo = new Account(clTwo.name, clTwo.secondName, clTwo.surname, clTwo.age, clTwo.dateOfBirth,1,12000,"Regular",1234);
+        accTwo.getClientInfo();
+        accTwo.updtAccBal(1234,329.99);
+        accTwo.updtPin(1234,4321);
+        Client clThree = new Client("Adrian","","Ciak",18,"2006-03-03");
+        Account accThree = new Account(clThree.name, clThree.secondName, clThree.surname, clThree.age, clThree.dateOfBirth,1,12000,"Regular",1234);
+        accThree.getClientInfo();
+        accThree.updtAccBal(1234,329.99);
+        accThree.updtPin(1234,4321);
     }
 }
