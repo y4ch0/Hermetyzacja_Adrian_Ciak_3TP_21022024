cfa33612625717ef39e529fa723dd97feb6f87f5
y4ch0 <adrianciak10@gmail.com>
2024-02-21 10:28:33 +0100

    modify some files

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Client clOne = new Client("Adrian","","Ciak",18,"2006-03-03");
-        Account accOne = new Account(clOne.name, clOne.secondName, clOne.surname, clOne.age, clOne.dateOfBirth,1,1000,"Regular",1234);
+        Account accOne = new Account(clOne.name, clOne.secondName, clOne.surname, clOne.age, clOne.dateOfBirth,1,12000,"Regular",1234);
         accOne.getClientInfo();
-        accOne.updtAccBal(1234,100);
+        accOne.updtAccBal(1234,329.99);
         accOne.updtPin(1234,4321);
     }
 }
